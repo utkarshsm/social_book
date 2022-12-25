4f500a3ea69ddbda6875cc3ea6c096fcc4f8e92e
utkarshsm <mukkawar.utkarsh2120@gmail.com>
2022-12-25 17:01:42 +0530

    Day2 Complete

--- abs_user/users/views.py
+++ abs_user/users/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import CustomUser
 # Create your views here.
+def home(request):
+    # user_data = CustomUser.objects.all()
+    user_data = CustomUser.objects.filter(public_visibility=1)
+    print(user_data)
+    return render(request,'users/home.html',{'CustomUser':user_data})
--- file_upload/file_upload/urls.py
+++ file_upload/file_upload/urls.py
@@ -1,7 +1,7 @@
-"""abs_user URL Configuration
+"""file_upload URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,15 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from users import views
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home)
+    path('', include('upload.urls')),
 ]
 
 if settings.DEBUG:
-    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
